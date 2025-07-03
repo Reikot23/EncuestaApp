796a36ed124aa233c705ab473941a68600b700b7
Reikot <victor.marinai@correoaiep.cl>
2025-07-03 14:09:37 -0400

    Agregado formulario de encuesta y guardado en archivo txt

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/resultado', methods=['POST'])
+def resultado():
+    nombre = request.form['nombre']
+    opinion = request.form['opinion']
+    return f"<h1>Gracias, {nombre} </h1><p>por tu opinión: {opinion}.</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
